784e5aa3b4d4ed9cff8239f808f8c53d2715dce2
ouweiya <higogle1@gmail.com>
2025-12-17 14:34:04 +0800

    2563

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,12 @@
 import type { NextConfig } from 'next';
 
-const repo = 'next.js-1';
+// const repo = 'next.js-1';
 
 const nextConfig: NextConfig = {
     output: 'export',
 
     basePath: '/next.js-1',
-    assetPrefix: '/next.js-1',
-    distDir: '/next.js-1',
+    // distDir: '/next.js-1',
     // trailingSlash: true,
 };
 
